1d5716ec704b586de174d0208b1b2997bed2fd83
Lucas Nunes o/ <lucasrafaelnunees@gmail.com>
2023-04-14 00:04:00 -0300

    Aulas: ngIf e Removendo item

---
+++ Projetos/Projetos/ProjetoInicial/src/Models/tarefas.model.ts
@@ -0,0 +1,8 @@
+// É desta forma que criamos uma classe publica.
+export class Tarefas {
+    constructor(
+        public id: Number,
+        public tarefas: String,
+        public done: Boolean,
+    ){}
+}

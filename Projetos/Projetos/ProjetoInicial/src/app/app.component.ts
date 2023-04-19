aecb881a94eab0940cf3f2478ce1202cfc547622
Lucas Nunes o/ <lucasrafaelnunees@gmail.com>
2023-04-19 00:43:00 -0300

    Aulas: Exibindo Validações, Desabilitando botão, Adicionando Tarefa, Salvando Itens

--- Projetos/Projetos/ProjetoInicial/src/app/app.component.ts
+++ Projetos/Projetos/ProjetoInicial/src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { getLocaleDateFormat, getLocaleDayNames } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Tarefas } from 'src/Models/tarefas.model'; //Realizada a importação da classe que iremos utilizar.
 
 @Component({
@@ -12,10 +13,10 @@ export class AppComponent {
 
   //Podemos criar variavel publica do tipo any, é uma array (lista).
   //Desta forma, criamos a variavel e tipando ela com a nossa classe.
-  public variavelExemplo: Tarefas[] = [];
+  public listTarefas: Tarefas[] = [];
 
   //Visualizar os exemplos de variaveis
-  public visualizarVariaveis: string = 'Tarefas Diárias';
+  public visualizarTarefas: string = 'Tarefas Diárias';
 
   //Criando o formsGroup
   public form: FormGroup;
@@ -25,30 +26,58 @@ export class AppComponent {
 
     //Instanciando o form:
     this.form = this.fb.group({ //Aqui estou criando um group dentro do formBuilder
-      title: ['', Validators.compose([
+      titleForm: ['', Validators.compose([
         Validators.minLength(3),
         Validators.maxLength(60), // Estou definindo que o nosso formulario o titulo, tem que ter minimo 3 caracteres maximo 60.
-        Validators.required //E aqui estou definindo que ele é requerido, obrigatorio.
+        Validators.required, //E aqui estou definindo que ele é requerido, obrigatorio.
       ])]
-    })
+    });
 
-    this.variavelExemplo.push(new Tarefas(1, 'Trabalhar horario comercial', false));
-    this.variavelExemplo.push(new Tarefas(2, 'Ir no mercado', false));
-    this.variavelExemplo.push(new Tarefas(3, 'Ir para a academia', true));
-    this.variavelExemplo.push(new Tarefas(4, 'Estudar', false));
+    // Agora já conseguimos adicionar a tarefa através do método add().
+    //this.listTarefas.push(new Tarefas(1, 'Trabalhar horario comercial', false));
+    //this.listTarefas.push(new Tarefas(2, 'Ir no mercado', false));
+    //this.listTarefas.push(new Tarefas(3, 'Ir para a academia', true));
+    //this.listTarefas.push(new Tarefas(4, 'Estudar', false));
+  }
+
+  add() {
+    //Primeiro iremos recuperar o valor que foi setado dentro de titleForm
+    const titleForm = this.form.controls['titleForm'].value;
+
+    //Adicionando conforme itens da lista +1
+    const id = this.listTarefas.length +1;
+
+    //Adicionando a tarefa a lista.
+    this.listTarefas.push(new Tarefas(id,titleForm,false));
+
+    //chamando o método de salvar
+    this.save();
+
+    //Chamo o método que limpa o forms depois de inserir na lista
+    this.clear()
   }
 
   //No caso o método vai esperar um tipo Tarefas.
   remove(list : Tarefas){
     //Desta forma eu irei saber o index que o todo esta me passando, para poder remover da lista.
     //Se caso não existir ele retorna -1
-    const index = this.variavelExemplo.indexOf(list);
+    const index = this.listTarefas.indexOf(list);
 
     //validação de segurança
     if(index !== -1){
       //Aqui eu estou passando o index que quero e quantos eu quero deletar.
-      this.variavelExemplo.splice(index, 1);
+      this.listTarefas.splice(index, 1);
     }
+  }
+
+  save() {
+    // O json.stringify converte um objeto json para string.
+    const data = JSON.stringify(this.listTarefas);
+
+    //Salvando em LocalStorage e ele pede chave e valor
+    //chave = listTarefas
+    //valor = data(meu objeto json convertido para string)
+    localStorage.setItem('listTarefas', data)
   }
 
   // Aqui eu já recebo o objeto como referencia, por isso não preciso utilizar o this.
@@ -61,4 +90,7 @@ export class AppComponent {
     list.done = false;
   }
 
+  clear() {
+    this.form.reset();
+  }
 }
